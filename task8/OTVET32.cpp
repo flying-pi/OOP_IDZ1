5d7024f6b9d74bb92bfb45836e18f35afe7bf91b
Yura Braiko <yurabraiko@gmail.com>
2016-11-17 06:40:51 +0200

    complete task 8

---
+++ task8/OTVET32.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Yura Braiko on 17.11.16.
+//
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void getbin(unsigned int,char *out);
+
+void otvet(const char *str,
+           const unsigned int &val3,
+           const unsigned int &val1,
+           const unsigned int &val2) {
+    char *tempstr = "INV      ";
+
+    ofstream out("./out.txt",std::ofstream::out | std::ofstream::app);
+    char *binaryValue = new char[32];
+    out << ("ОТВЕТ:\n");
+    if (strcmp(str, tempstr) != 0) {
+        out << setfill(' ')<<setw(18) << dec << val1 << " 0x"<<setfill('0')<<setw(8)<<hex<<val1<< " ";
+        getbin(val1,binaryValue);
+        out <<binaryValue;
+        out << str << setfill(' ')<<setw(10) << dec << val1 << " 0x"<<setfill('0')<<setw(8)<<hex<<val1<< " ";
+        getbin(val2,binaryValue);
+        out <<binaryValue;
+        out << "\n===================================\n";
+        out << setfill(' ')<<setw(18) << dec << val3 << " 0x"<<setfill('0')<<setw(8)<<hex<<val3<< " ";
+        getbin(val3,binaryValue);
+        out <<binaryValue;
+    } else {
+        out << setfill(' ')<<setw(18) << dec << val1 << " 0x"<<setfill('0')<<setw(8)<<hex<<val1<< " ";
+        getbin(val1,binaryValue);
+        out <<binaryValue;
+        out << "\n===================================\n";
+        out << setfill(' ')<<setw(18) << dec << val3 << " 0x"<<setfill('0')<<setw(8)<<hex<<val3<< " ";
+        getbin(val3,binaryValue);
+        out <<binaryValue;
+    }
+    out << "\n";
+    out.close();
+    delete[] binaryValue;
+}
