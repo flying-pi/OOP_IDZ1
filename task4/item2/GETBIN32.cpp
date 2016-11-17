e72377535aa33d7190f54a3ca8d45f6d59a7c0b7
Yura Braiko <yurabraiko@gmail.com>
2016-11-17 04:12:19 +0200

    complete task 4.2

---
+++ task4/item2/GETBIN32.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Yura Braiko on 17.11.16.
+//
+
+#include <stdio.h>
+
+void getbin(unsigned int *n){
+    for(int i=0;i<32;i++){
+        if((*n)&0x80000000)
+            putc('1',stdout);
+        else
+            putc('0',stdout);
+        (*n)=(*n)<<1;
+    }
+}
